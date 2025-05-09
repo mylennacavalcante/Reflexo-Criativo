f0971e8ebd982145f0d98af4a7c06862dab28d9a
Mylenna Cavalcante <mylennacelestino@hotmail.com>
2025-05-09 17:51:37 -0300

    versão 14

--- script/login.js
+++ script/login.js
@@ -1,6 +1,12 @@
 const login = document.querySelector('.bntLogin');
-const caixaLogin = document.querySelector('#contaLogin');
+const fechar = document.querySelector('.fechar');
+const caixaLogin = document.querySelector('.ocultoLogin');
 
 login.addEventListener("click", () => {
     caixaLogin.classList.remove("ocultoLogin")
+})
+
+
+fechar.addEventListener("click", () => {
+    caixaLogin.classList.add("ocultoLogin")
 })
